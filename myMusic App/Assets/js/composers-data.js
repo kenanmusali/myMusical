f27690453b9ac3424564032a70205aa859da27a6
kenanmusali <textme0306@gmail.com>
2024-02-01 01:19:39 -0800

    1.6

--- myMusic App/Assets/js/composers-data.js
+++ myMusic App/Assets/js/composers-data.js
@@ -1,26 +1,30 @@
-let composerData = [
-    {
-      "composer": {
-        "composerProfile": {
-          "img": "./Images/Composers/SamuelBarber.png",
-          "name": "Samuel Barber",
-          "composerDate": "1910 - 1981",
-          "musicName": "Adagio for Strings",
-          "musicYear": "written in 1936",
-          "description": "Adagio for Strings creates an uneasy, shifting suspension as the melody begins a stepwise motion, like the hesitant climbing of stairs.",
-          "Chords": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
-          "Compositions": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
-        }
-      }
-    }
-    // composers if needed
-  ];
+// let composerData =  [{
+//   "id":1,
+//   "img": "./Images/Composers/SamuelBarber.png",
+//   "name": "Samuel Barber",
+//   "composerDate": "1910 - 1981",
+//   "musicName": "Adagio for Strings",
+//   "musicYear": "written in 1936",
+//   "description": "Adagio for Strings creates an uneasy, shifting suspension as the melody begins a stepwise motion, like the hesitant climbing of stairs.",
+//   "Chords": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
+//   "Compositions": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
+//   "compositionsList": "Adagio for Strings, Agnus Dei",
+//   "songs": "./Songs/Samuel Barber/x2mate.com - Samuel Barber - Adagio for Strings (320 kbps).mp3"
+// },{
+//   "id":2,
+//   "name":"Salam",
+//   "composerDate":"1990"
+// }]
+  
   
 
-
-
-for (let i = 0; i < composerData.length; i++) {
-    console.log(composerData[i]);
-
-}
-
+// composerData.map((item,index,id)=>{
+// if(item.id===1){
+//   document.getElementById('name').innerHTML= `<h1 class="composerName">${item.name}</h1>`
+// document.getElementById('date').innerHTML= `<p class="composerYear" id="date">${item.composerDate}</p>`
+// }
+// else if(item.id===2){
+//   document.getElementById('name').innerHTML= `<h1 class="composerName">${item.name}</h1>`
+// document.getElementById('date1').innerHTML= `<p class="composerYear" id="date">${item.composerDate}</p>`
+// }
+// })
