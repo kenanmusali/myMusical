3ece8b18d3160868a4a8354a88359143d63ad602
kenanmusali <textme0306@gmail.com>
2024-02-01 01:52:58 -0800

    1.6 (Music .JS created)

--- myMusic App/Assets/js/composers-data.js
+++ myMusic App/Assets/js/composers-data.js
@@ -1,22 +1,45 @@
-// let composerData =  [{
-//   "id":1,
-//   "img": "./Images/Composers/SamuelBarber.png",
-//   "name": "Samuel Barber",
-//   "composerDate": "1910 - 1981",
-//   "musicName": "Adagio for Strings",
-//   "musicYear": "written in 1936",
-//   "description": "Adagio for Strings creates an uneasy, shifting suspension as the melody begins a stepwise motion, like the hesitant climbing of stairs.",
-//   "Chords": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
-//   "Compositions": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
-//   "compositionsList": "Adagio for Strings, Agnus Dei",
-//   "songs": "./Songs/Samuel Barber/x2mate.com - Samuel Barber - Adagio for Strings (320 kbps).mp3"
-// },{
-//   "id":2,
-//   "name":"Salam",
-//   "composerDate":"1990"
-// }]
-  
-  
+let MusicData = {
+  "SamuelBarber": [
+    {
+      // "id": 1,
+      "img": "./Images/Composers/SamuelBarber.png",
+      "fullName": "Samuel Barber",
+      "composerDate": "1910 - 1981",
+      "musicName": "Adagio for Strings",
+      "musicYear": "written in 1936",
+      "description": "Adagio for Strings creates an uneasy, shifting suspension as the melody begins a stepwise motion, like the hesitant climbing of stairs.",
+      "Chords": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
+      "Compositions": "features a poignant arrangement for first violins, second violins, violas, cellos, and double basses.",
+      "compositionsList": "Adagio for Strings, Agnus Dei",
+      "songs": "./Songs/Samuel Barber/x2mate.com - Samuel Barber - Adagio for Strings (320 kbps).mp3"
+    }
+  ]
+};
+
+
+
+for (let i = 0; i < MusicData["SamuelBarber"].length; i++) {
+  console.log(MusicData["SamuelBarber"][i].fullName);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // composerData.map((item,index,id)=>{
 // if(item.id===1){
